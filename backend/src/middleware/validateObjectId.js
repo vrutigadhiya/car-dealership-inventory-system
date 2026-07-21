bc654775ce8d15636dec20a031959000a60a794f
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-21 23:55:54 +0530

    refactor: simplify vehicle deletion logic

---
+++ backend/src/middleware/validateObjectId.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const validateObjectId = (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid vehicle ID format",
+    });
+  }
+
+  next();
+};
+
+module.exports = validateObjectId;
--- backend/src/routes/vehicleRoutes.js
+++ backend/src/routes/vehicleRoutes.js
@@ -4,19 +4,20 @@ const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
+const validateObjectId = require("../middleware/validateObjectId");
 
 const {
   addVehicle,
   getVehicles,
   searchVehicle,
   updateVehicleById,
-  deleteVehicleById
+  deleteVehicleById,
 } = require("../controllers/vehicleController");
 
 router.post("/", authMiddleware, addVehicle);
 router.get("/", authMiddleware, getVehicles);
 router.get("/search", authMiddleware, searchVehicle);
-router.put("/:id", authMiddleware, updateVehicleById);
-router.delete("/:id", authMiddleware, adminMiddleware, deleteVehicleById);
+router.put("/:id", authMiddleware, validateObjectId, updateVehicleById);
+router.delete("/:id", authMiddleware, adminMiddleware, validateObjectId, deleteVehicleById);
 
 module.exports = router;
