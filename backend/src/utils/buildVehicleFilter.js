bbf5dd23993edbad65718e046d9422171ba0ae7c
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-21 22:59:04 +0530

    refactor: simplify vehicle search filtering

---
+++ backend/src/utils/buildVehicleFilter.js
@@ -0,0 +1,31 @@
+const buildVehicleFilter = (query) => {
+  const filter = {};
+
+  if (query.make) {
+    filter.make = { $regex: query.make, $options: "i" };
+  }
+
+  if (query.model) {
+    filter.model = { $regex: query.model, $options: "i" };
+  }
+
+  if (query.category) {
+    filter.category = { $regex: query.category, $options: "i" };
+  }
+
+  if (query.minPrice || query.maxPrice) {
+    filter.price = {};
+
+    if (query.minPrice) {
+      filter.price.$gte = Number(query.minPrice);
+    }
+
+    if (query.maxPrice) {
+      filter.price.$lte = Number(query.maxPrice);
+    }
+  }
+
+  return filter;
+};
+
+module.exports = buildVehicleFilter;
