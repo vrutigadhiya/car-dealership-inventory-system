9911d6595ce07966612d1b5feb990bd39701c8d1
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-21 17:22:49 +0530

    feat: implement registration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 
+//Local Module 
+const authRoutes = require("./routes/authRoute")
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("API Running...");
