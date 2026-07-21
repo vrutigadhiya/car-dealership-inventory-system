16860e29288fecedf534af9f40aca81d280a2cfe
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-21 21:16:04 +0530

    test: add vehicle creation tests

---
+++ backend/src/models/Vehicle.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const vehicleSchema = new mongoose.Schema(
+  {
+    make: {
+      type: String,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Vehicle", vehicleSchema);
