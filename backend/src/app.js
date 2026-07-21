1249320a610865a41fb1e5fcb7f9db87bcde32a8
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-21 21:24:38 +0530

    feat: implement vehicle creation endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,22 +2,17 @@ require("dotenv").config();
 const express = require("express");
 
 //Local Module 
-const authRoutes = require("./routes/authRoute")
+const authRoutes = require("./routes/authRoutes")
 const errorHandler = require("./middleware/errorHandler");
-const authMiddleware = require("./middleware/authMiddleware");
+const vehicleRoutes = require("./routes/vehicleRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/vehicles", vehicleRoutes);
 app.use(errorHandler);
-app.post("/api/vehicles", authMiddleware, (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Protected route accessed",
-  });
-});
 
 app.get("/", (req, res) => {
     res.send("API Running...");
