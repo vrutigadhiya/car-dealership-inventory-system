5998127774e063cdeff5aae8c91baaaaee3d8837
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-21 17:22:49 +0530

    feat: implement registration

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,42 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User Registered Successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
