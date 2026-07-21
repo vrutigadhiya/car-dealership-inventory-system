78beb89b46ba8a2119bb6d74b9c967b8ea9861c3
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-21 23:55:54 +0530

    refactor: simplify vehicle deletion logic

--- backend/src/controllers/vehicleController.js
+++ backend/src/controllers/vehicleController.js
@@ -77,13 +77,6 @@ const deleteVehicleById = async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid vehicle ID format",
-      });
-    }
-
     const vehicle = await deleteVehicle(id);
 
     if (!vehicle) {
