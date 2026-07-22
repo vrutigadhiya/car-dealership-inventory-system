aa029bd45e086c4ec154eaf0ef7b8498088882ff
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2026-07-22 09:22:21 +0530

    feat: set up React Router

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Admin Dashboard</h1>
+    </div>
+  );
+}
+
+export default AdminDashboard
